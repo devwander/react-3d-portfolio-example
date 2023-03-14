0ef40b29fc495681e2f6087e06c22558a1c31cb8
devwander <josewanderson173@gmail.com>
2023-03-14 20:15:42 -0300

    Made 3d animation of the Who

---
+++ src/components/Cube.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react"
+import { OrbitControls, PerspectiveCamera, RenderTexture, Text } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
+
+const Cube = () => {
+    const textRef = useRef()
+    useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime)))
+    return (
+        <mesh>
+            <boxGeometry />
+            <meshStandardMaterial>
+                <RenderTexture attach="map">
+                <PerspectiveCamera 
+                    makeDefault
+                    position={[0, 0, 5]}
+                />
+                <color attach="background" args={["rebeccapurple"]} />
+                <Text ref={textRef} fontSize={2} color="black">
+                    Hello
+                </Text>
+                </RenderTexture>
+            </meshStandardMaterial>
+        </mesh>
+    )
+}
+
+export default Cube
